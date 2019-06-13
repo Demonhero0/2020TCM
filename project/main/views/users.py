0c4d137955f7e342ee5067ebb30885c43bb7093b
Nouzan <clobraindie@outlook.com>
2019-06-13 10:57:12 +0800

    add non-docker envions

--- project/main/views/users.py
+++ project/main/views/users.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+
 from project.main.serializers.users import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
