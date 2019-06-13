d35cd14c2b612f02c3ddb2aee1a54b4b8cac96b0
Nouzan <clobraindie@outlook.com>
2019-06-13 11:28:51 +0800

    Update UserSerializer
    - add password field

--- project/main/serializers/users.py
+++ project/main/serializers/users.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('url', 'username', 'email', 'password')
+
+    def save(self, **kwargs):
+        if 'password' in self.validated_data:
+            self.validated_data['password'] = make_password(self.validated_data['password'])
+
+        super(UserSerializer, self).save(**kwargs)
