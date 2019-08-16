6a381489cb40adba09caef51ef405023ba442000
Nouzan <clobraindie@outlook.com>
2019-08-16 12:02:23 +0800

    add logout API

--- project/main/views/users.py
+++ project/main/views/users.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
 
 from project.main.serializers.users import UserSerializer
 
@@ -13,3 +17,15 @@ class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'])
+    def logout(self, request, pk=None):
+        if pk:
+            for token in Token.objects.filter(user=pk):
+                token.delete()
+        
+        return Response({
+            'code': 0,
+            'message': "成功",
+            'id': pk
+        })
