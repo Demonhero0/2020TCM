22b6936de0a2e1d09eb8dc6bdba9e8e785f065ac
Demonhero0 <546584293@qq.com>
2020-12-22 15:05:25 +0800

    change post to get in index.html

--- project/main/views/doctor.py
+++ project/main/views/doctor.py
@@ -23,15 +23,31 @@ class MedicineViewSet(viewsets.ModelViewSet):
 class CheckViewSet(APIView):
 
     def get(self,request):
-        disease_query = Disease.objects.all().order_by('-hits')[:3]
-        res = DiseaseSerializer(disease_query,context={"request":request}, many=True)
-        return render(request, 'index.html', {'res':res.data})
+        content = request.GET.get('content',[])
+        print(content)
+        if content:
+            tags = jieba.analyse.extract_tags(content, topK=20, withWeight=True, allowPOS=('ns','n','v'))
+            print(tags)
+            disease_query = Disease.objects.none()
+            for tag in tags:
+                disease_query = disease_query.union(Disease.objects.filter(symptom__contains=tag[0]))
+                disease_query = disease_query.union(Disease.objects.filter(name=tag[0]))
+            print(disease_query)
+            res = DiseaseSerializer(disease_query,context={"request":request}, many=True)
+            print(res)
+            return render(request, 'result.html', {'res':res.data, "content":content})
+            # return Response({'msg': res.data}, status=status.HTTP_200_OK)
+        else:
+            disease_query = Disease.objects.all().order_by('-hits')[:3]
+            res = DiseaseSerializer(disease_query,context={"request":request}, many=True)
+            return render(request, 'index.html', {'res':res.data})
+        return Response({'msg': 'do nothing'}, status=status.HTTP_200_OK)
         # return Response({'msg': 'use post request'}, status=status.HTTP_200_OK, template_name='index.html')
 
     # example {"content":"经常性头晕，频繁肚子痛"}
     # {"content":"流鼻涕，有点发热，恶心"}
     def post(self,request,format=None):
-        print(request.data)
+        # print(request.data)
         content = request.data.get('content',[])
         if content:
             tags = jieba.analyse.extract_tags(content, topK=20, withWeight=True, allowPOS=('ns','n','v'))
